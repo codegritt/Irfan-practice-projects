2a0ec06e576bf5dc1b6b0360b50bd4ecbc6a2e10
Gokul <gokul@saaragh.in>
2023-08-22 18:18:06 +0530

    Obj, arr destructuring

--- vite-project/src/Components/ArrayDestructuring.tsx
+++ vite-project/src/Components/ArrayDestructuring.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+const employees = {
+  name: "gokul",
+  age: "24",
+  address: {
+    city: "chicago",
+    Country: "usa",
+  },
+};
+
+const { name: fullname, age = 30 } = employees;
+
+const names = ["gokul", "max", "john"];
+const names1 = ["gokulqwe", "maxqwe", "johnqwe"];
+const [x, y, z] = names;
+const [a, ...b] = names1;
+
+console.log(a);
+console.log(b);
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log(fullname);
+console.log(age);
+
+const slaves = [
+  {
+    name: "Saka Manje",
+    address: "14, cassava-garri-ewa street",
+    gender: "Male",
+  },
+  {
+    name: "Wawawa Warisii",
+    address: "406, highway street",
+    gender: "Male",
+  },
+];
+
+const shoppingList = ["Oranges", "Cassava", "Garri", "Ewa", "Dodo", "Books"];
 
 const ArrayDestructuring = () => {
   return (
-    <div>ArrayDestructuring</div>
-  )
-}
+    <>
+      {shoppingList.map((item, index) => {
+        return (
+          <ol>
+            <li key={index}>{item}</li>
+          </ol>
+        );
+      })}
 
-export default ArrayDestructuring
+      {slaves.map((slave, index) => {
+        return (
+          <div key={index}>
+            <p>{slave.name}</p>
+            <p>{slave.address}</p>
+            <p>{slave.gender}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ArrayDestructuring;
